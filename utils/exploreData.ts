b7dc9b900d7848c021b75f9187d17a2724fb3a5b
xueyong <1085523029@qq.com>
2023-03-14 15:04:01 +0800

    ✨ s3 upload

--- utils/exploreData.ts
+++ utils/exploreData.ts
@@ -4,16 +4,19 @@ import Color from "@/assets/Color.json"
 import PinyinConvertSelection from "@/assets/Pinyin Convert Selection.json"
 import Pinyin from "@/assets/Pinyin.json"
 import RGB from "@/assets/RGB.json"
-// JSON parse error
-import S3 from "@/assets/S3.js"
 import TinypngCompressImageFile from "@/assets/Tinypng Compress Image File.json"
 import TinypngCompressImageLink from "@/assets/Tinypng Compress Image Link.json"
 import TinypngFile from "@/assets/Tinypng File.json"
 import Tinypng from "@/assets/Tinypng.json"
 import ZeplinImg from "@/assets/ZeplinImg.json"
 
+// JSON parse error
+import S3Upload from "@/assets/S3 Upload"
+import S3 from "@/assets/S3.js"
+
 export const data = [
   S3,
+  S3Upload,
   Tinypng,
   Base64DecodeSelection,
   Base64,
