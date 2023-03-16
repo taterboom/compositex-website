47a1a528c58bce610f1d7ca820c22ad35b022fcd
xueyong <1085523029@qq.com>
2023-03-16 11:02:47 +0800

    🐛 change attr name

--- app/page.tsx
+++ app/page.tsx
@@ -47,9 +47,9 @@ export default function Home() {
             height="450"
             src="https://www.youtube.com/embed/CJKqKhAKlNk"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
         <div ref={pinRef}>
